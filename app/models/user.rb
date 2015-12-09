802867b869590275af307aae8f36bf1769fadaa1
alejo <alejorodasvasquez@gmail.com>
2015-12-09 15:50:26 -0500

    se agrega el metodo add_publications al modelo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :publications
   has_many :articles, through: :publications
+
+  def add_publications articles_published
+    articles_published.each do |article|
+      publications.build(article)
+    end
+  end
+
 end
